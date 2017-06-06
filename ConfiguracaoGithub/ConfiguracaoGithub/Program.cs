2386f26d3267e432b170878f77cea884db6f9624
DiogoPCS <diogo.paulino.santos@hotmail.com>
2017-06-06 01:28:22 -0300

    Primeiro acesso

---
+++ ConfiguracaoGithub/ConfiguracaoGithub/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfiguracaoGithub
+{
+    class Program
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public double salario { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+
+
+        }
+    }
+}
